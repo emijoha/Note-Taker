fefe79d8abb61c3930ea0e152b1d55cc65a05a1d
emi <ejhartline@gmail.com>
2020-05-03 13:52:15 -0400

    updated commenting on apiRoutes.js for better understanding

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,52 +7,62 @@ const dbPath = path.join(__dirname, "../db/db.json");
 // export API routing
 module.exports = app => {
   // API GET Requests, to access all saved notes
-  // Should read the `db.json` file and return all saved notes as JSON.
     app.get("/api/notes", (req, res) => {
-    
+
+        // Returns all saved notes
         res.sendFile(dbPath);
 
     });
 
   // API POST Requests, to save data sent to server
-  // Should receive a new note to save on the request body, 
-  // add it to the `db.json` file, and then return the new note to the client.
     app.post("/api/notes", (req, res) => {
+
+        // New note from request body
         const newNote = req.body;
 
         fs.readFile(dbPath, (err, data) => {
-            if (err) throw err;
+            if (err) throw err;\
+
+            // Data from file is parsed and stored in new variable
+            // New note is pushed into noteData
             const noteData = JSON.parse(data);
             noteData.push(newNote);
+
+            // Uses updated data to over-write db.json file
             fs.writeFile(dbPath, JSON.stringify(noteData), err => {
                 if (err) throw err;
             });
         });
 
+        // Returns the new note to the client.
         res.json(newNote);
     });
 
-  //  API DELETE Request, to delete a note 
-  // Should receive a query parameter containing the id of a note to delete. 
-  // This means you'll need to find a way to give each note a unique `id` when it's saved. 
-  // In order to delete a note, you'll need to read all notes from the `db.json` file, 
-  // remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+  // API DELETE Request, to delete a note
     app.delete("/api/notes/:id", (req, res) => {
+
+        // Receives a query parameter containing the id of a note to delete. 
         const id = req.params.id;
 
+        // Reads all notes from the `db.json` file
         fs.readFile(dbPath, (err, data) => {
             if (err) throw err;
 
+            // Data from file is parsed and stored in new variable
             const noteData = JSON.parse(data);
 
+            // Removes the note with the given `id` property
+            // This is done by creating new variable that holds all notes EXCEPT that one with the given id
             const undeletedNotes = noteData.filter(note => note.id !== id);
 
+            // Rewrites `db.json` file with updated note data
             fs.writeFile(dbPath, JSON.stringify(undeletedNotes), err => {
                 if (err) throw err;
             });
 
         });
 
+        // Returns updated file of all undeleted notes
         res.sendFile(dbPath);
 
     });
